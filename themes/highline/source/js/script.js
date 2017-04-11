ffde64c8583ab54b19b7bfe609c136d624b235c7
James Cole <mail@ofjamescole.com>
2017-04-10 20:36:46 -0400

    Calculate font size in JS because Safari can't do vw in calc

--- themes/highline/source/js/script.js
+++ themes/highline/source/js/script.js
@@ -1,23 +1,41 @@
 (function(JC, $) {
-  document.addEventListener('DOMContentLoaded', function(e) {
-    setTimeout(function() {      
-      // apply this css here so
-      // the page has time to set
-      // the layout before fixing
-      // the header
-      $('.header').addClass('is-ready');
-      $('.header__space').addClass('is-ready');
-    }, 100);
+	document.addEventListener('DOMContentLoaded', function(e) {
+		setTimeout(function() {
+			// apply this css here so
+			// the page has time to set
+			// the layout before fixing
+			// the header
+			$('.header').addClass('is-ready');
+			$('.header__space').addClass('is-ready');
+		}, 100);
 
-    if (JC.HomeAnimations) {
-      var homeAnimations = new JC.HomeAnimations();
-      homeAnimations.init();
-    }
+		if (JC.HomeAnimations) {
+			var homeAnimations = new JC.HomeAnimations();
+			homeAnimations.init();
+		}
 
-    $('.js-menu-trigger').on('click', function(e) {
-      e.preventDefault();
-      $(this).toggleClass('is-active');
-      $('.js-main-nav').toggleClass('is-active');
-    });
-  });
+		$('.js-menu-trigger').on('click', function(e) {
+			e.preventDefault();
+			$(this).toggleClass('is-active');
+			$('.js-main-nav').toggleClass('is-active');
+		});
+
+		function responsiveFontSize() {
+			var width = $(window).width();
+			if (width < 1024 && width > 320) {
+				var size = 16 + .5 * ((width - 320) / 44);
+				$('html').css({
+					fontSize: size / 16 + 'rem'
+				});
+			} else {
+				$('html').removeAttr('style');
+			}
+		}
+
+		responsiveFontSize();
+
+		window.addEventListener('resize', JC.utils.debounce(function() {
+			responsiveFontSize();
+		}, 50), !1);
+	});
 })(JC, jQuery, undefined);
--- themes/highline/source/js/utility.js
+++ themes/highline/source/js/utility.js
@@ -1,16 +1,17 @@
 (function($) {
-  var utils = {
-    debounce: function debounce(fn, delay) {
-  var timer = null;
-  return function () {
-    var context = this, args = arguments;
-    clearTimeout(timer);
-    timer = setTimeout(function () {
-      fn.apply(context, args);
-    }, delay);
-  };
-}
-  }
+	var utils = {
+		debounce: function debounce(fn, delay) {
+			var timer = null;
+			return function() {
+				var context = this,
+					args = arguments;
+				clearTimeout(timer);
+				timer = setTimeout(function() {
+					fn.apply(context, args);
+				}, delay);
+			};
+		}
+	}
 
-  return JC.utils = utils;
+	return JC.utils = utils;
 })(jQuery, undefined);
